c605ebf7d5a27ff256a61f76406fd658e26865de
prokopiev@100up.ru <slava200207>
2023-05-15 14:46:22 +0300

    merge

---
+++ cis/cis_9.R
@@ -0,0 +1,33 @@
+library(dplyr)
+dates <- seq(as.Date("2023-01-01"), as.Date("2023-04-10"), by = "days")
+values <- sample(300:700, size = 100)
+df <- data.frame(date = dates,
+                 orders = values)
+write.csv(df, "./cis.csv", row.names = F)
+df <- df[order(df$orders),]
+
+X_sample <- df$orders
+
+N <- length(X_sample) # Number of members in X_sample
+n_groups = (700 - 300 ) / 40
+
+
+h <- 40 # Hist step
+Z <- seq(from = min(X_sample),
+         to = max(X_sample),
+         by = h) # Hist boundaries
+
+
+
+X <- (Z[-1] + Z[-length(Z)]) / 2 # Hist centers
+table_cut <- data.frame(values = X_sample,
+                        groups = cut(X_sample, n_groups)) 
+
+
+table_hist <- table_cut %>%
+  dplyr::group_by(groups) %>%
+  dplyr::summarise(abs_freq = n(),
+                   rel_freq = n()/N)
+
+table_hist[1:2]
+H <- -sum(table_hist$rel_freq * log2(table_hist$rel_freq))
